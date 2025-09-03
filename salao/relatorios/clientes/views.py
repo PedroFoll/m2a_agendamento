907ec8d7124b0eb170ffc045519ee89d0a3b4ca3
Pedro Foll <pedro.fol29@gmail.com>
2025-09-03 11:03:29 -0300

    Feat: Criação da opção de deletar usuário e agendamento, visualização de usuário e agendamento, além dos relatorios.

--- salao/relatorios/clientes/views.py
+++ salao/relatorios/clientes/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from servicos.agendamento.models import Agendamento
 from cadastros.cliente.models import Cliente
 
 
 
-def relatorio(request):
+def relatorio_agendamentos(request):
     limit = 15
     pagina = int(request.GET.get('pagina', 1))
     agendamentos = Agendamento.objects.all()
@@ -30,8 +30,60 @@ def relatorio(request):
         'limit': limit,
     }
 
+    return render(request, "agendamentos/relatorio_agendamentos.html", contexto)
+
+def relatorio_clientes(request):
+    limit = 15
+    pagina = int(request.GET.get('pagina', 1))
+    clientes = Cliente.objects.all()
+    qntd_clientes = clientes.count()
+    nome_filtrar=request.GET.get('nome_filtrar')
+    email_filtrar=request.GET.get('email_filtrar')
+    tipo_usuario_filtrar= request.GET.get('tipo_usuario_filtrar')
+
+    clientes = Cliente.objects.all()
+
+    if nome_filtrar:
+        clientes = clientes.filter(nome__contains=nome_filtrar)
+    
+    if email_filtrar:
+        clientes = clientes.filter(email__contains=email_filtrar)
+    
+    if tipo_usuario_filtrar:
+        clientes = clientes.filter(tipo_usuario__iexact=tipo_usuario_filtrar.lower())
+
+    offset = (pagina - 1) * limit
+    clientes = clientes.order_by('nome')[offset:offset + limit]
+
+    contexto = {
+        'clientes': clientes,
+        'qntd_clientes': qntd_clientes,
+        'pagina': pagina,
+        'limit': limit,
+    }
+
+
     return render(request, "clientes/relatorio_clientes.html", contexto)
+
+
 
 def ver_cliente(request,id):
     cliente=Cliente.objects.get(id=id)
-    return  render(request, 'clientes/ver_cliente.html', {'cliente': cliente})
+    return  render(request, "clientes/ver_clientes.html", {'cliente': cliente})
+
+
+def ver_agendamento(request, id):
+    agendamento=Agendamento.objects.get(id=id)
+    return  render(request, "agendamentos/ver_agendamento.html", {'agendamento': agendamento})
+
+
+
+def deletar_cliente(request,id):
+    cliente=Cliente.objects.get(id=id)
+    cliente.delete()
+    return redirect('/relatorios/clientes/')
+
+def deletar_agendamento(request, id):
+    agendamento = Agendamento.objects.get(id=id)
+    agendamento.delete()
+    return redirect('/relatorios/agendamentos/')
--- salao/relatorios/urls.py
+++ salao/relatorios/urls.py
@@ -4,8 +4,13 @@ from .clientes import views
 
 app_name ='relatorios'
 urlpatterns =[
-    path('', views.relatorio, name='relatorio'),
+    path('clientes/', views.relatorio_clientes, name='relatorio_clientes'),
+    path('agendamentos/', views.relatorio_agendamentos, name='relatorio_agendamentos'),
     path('ver_cliente/<int:id>',views.ver_cliente, name='ver_cliente'),
+    path('ver_agendamento/<int:id>', views.ver_agendamento, name='ver_agendamento'),
+    
+    path('deletar_cliente/<int:id>',views.deletar_cliente, name='deletar_cliente'),
+    path('deletar_agendamento/<int:id>', views.deletar_agendamento, name='deletar_agendamento'),
 
     #path('cadastro_gestor_cliente/', views.cadastro_gestor_cliente, name='cadastro_gestor_cliente')
 ]
